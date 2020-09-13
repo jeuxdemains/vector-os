8eb5c7e1ed7460bc4eac21ce622b72f683372849
Yuriy Georgiev <u.georgiev@gmail.com>
2020-09-13 15:26:26 -0400

    implemented VGA buffer boundary check when printing strings; implemented XY positioning of string prints

--- string.c
+++ string.c
@@ -1,14 +1,42 @@
+int SCRN_POS = VIDEO_OFFSET;
+
+void StringSetPos(int x, int y)
+{
+    SCRN_POS = (char*)VIDEO_OFFSET;
+
+    for (int i = 0; i < x; ++i)
+        SCRN_POS += 2;
+
+    for (int j = 0; j < y; ++j)
+        SCRN_POS += 80*2;
+}
+
+void StringResetPos()
+{
+    SCRN_POS = (char*)VIDEO_OFFSET;
+}
+
 void StringPrint(const char* str)
 {
-	char* vidMem = (char*)VIDEO_OFFSET;
+    //make a check if we are withing the VGA buffer, if not set it up
+    if (SCRN_POS < VIDEO_OFFSET || SCRN_POS > VIDEO_OFFSET + (80*25))
+        StringResetPos();
 
-	int cnt = 0;
-	while(str[cnt] != 0x0)
+	char* vidMem = (char*)SCRN_POS;
+
+	int letterIndex = 0;
+	while(str[letterIndex] != 0x0)
 	{
-		*vidMem = str[cnt];
-		++cnt;
+		*vidMem = str[letterIndex];
+		++letterIndex;
 		++vidMem;
 		*vidMem = VideoGetTeletypeColor();
 		++vidMem;
 	}
+}
+
+void StringPrintPos(const char* str, int x, int y)
+{
+    StringSetPos(x, y);
+    StringPrint(str);
 }
