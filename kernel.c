2fb5b146dd77305ba4cca949cb3eda15da77df67
Yuriy Georgiev <u.georgiev@gmail.com>
2020-09-18 00:38:01 +0300

    implemented 32 bit mode USB legacy support for PS/2 keyboard input emulation

--- kernel.c
+++ kernel.c
@@ -1,6 +1,7 @@
 #include "video.c"
 #include "screen.c"
 #include "string.c"
+#include "kbd.c"
 
 void main ()
 {
@@ -12,5 +13,14 @@ void main ()
 	//StringResetPos();
 	StringPrint("-\\- A simple OS bootloader and kernel coded in ASM & C -/-\0");
 	StringPrintPos("an X, Y (x:13, y:8) positioned string", 13, 8);
+    
+    while(1)
+    {
+        uint8_t byte;
+        while(byte = scan())
+        { 
+            StringPrintPos("input received", 13, 11);
+        }
+    }
 }
 
